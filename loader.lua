4b51ad61dcb3128b1fd1d19a7fc7dffc39baa854
Soggy-Lua <jdkelley747@gmail.com>
2025-07-31 20:29:31 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,7 @@
+-- loader.lua
 
+local baseUrl = "https://raw.githubusercontent.com/Soggy-Lua/Rblx-Src/refs/heads/main/"
+
+loadstring(game:HttpGet(baseUrl .. "init.lua", true))()
+
+loadstring(game:HttpGet(baseUrl .. "main.lua", true))()
